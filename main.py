c5ae549d0e81157d0c6c95ee6efd08b6377d3103
lucacoratu <lucacoratu@yahoo.com>
2022-05-19 14:41:06 +0300

    Final state of the project (added the calculation for the probability of the svm to guess right).

--- main.py
+++ main.py
@@ -18,8 +18,5 @@ def main():
 
     livedet.PlotWrongPredictions()
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
